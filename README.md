8cab5dbb2abf5c530590e53413216d6948908c15
kevinho <merlingithuub@gmail.com>
2022-08-20 00:14:05 -0300

    backup

---
+++ README.md
@@ -0,0 +1,76 @@
+# This telegram bot can bypass stinky and irritating url shortner with in a sec or two
+
+
+## contributers
+[Bruce](https://Brooklyn888@github.com) - Owner Of Bypass Bot Repo.
+
+#Warning
+Those Who are Forking This Repo,Be Careful.Its my private repo.I modified by my self.
+
+
+## Supported Links
+- adf.ly link
+- droplink.co link
+- gplink stinky url
+- AppDrive or DriveApp links (login required wont work for now)
+- linkvertise.com
+- exe.io/exey.io
+- sub2unlock.net/sub2unlock.com
+- rekonise.com
+- letsboost.net
+- ph.apps2app.com
+- mboost.me
+- shortconnect.com
+- sub4unlock.com
+- ytsubme.com
+- bit.ly
+- social-unlock.com
+- boost.ink
+- goo.gl
+- shrto.ml
+- t.co
+- tinyurl.com
+
+# bot father commands list
+```javascript
+start - Welcome Message
+
+adf - Bypass adf.ly links
+droplink -  Bypass droplink.co links
+sd - Bypass AppDrive or DriveApp links (login required)
+lv -  Bypass Linkvertise links
+ex - Bypass Other Links
+help - Commands List
+```
+
+# Intial Deploy to heroku
+Intially I was not able to push a simple bot to heroku, but yeah googling a lot made it work!
+
+## Very Initial Steps if you wanna host in your vps or local machine[NOOB Steps]
+
+1. Install Telegram :)
+2. Create a telegram bot by talking to [Bot Father]
+3. Install python in your computer, if you are on windows 
+4. Download my repo or clone it using git clone
+5. Edit TOKEN.txt and add your bot token
+6. For linux edit main.py and replace python with python3
+7. open cmd or terminal from that folder
+8. for windows or  (for Linux)
+
+```bash
+  run python main.py
+```
+for Linux
+```bash
+  run python3 main.py
+```
+
+### (Deploying to Heroku)
+- Fork this repo and edit TOKEN.txt and fill your bot token
+- use the link below and deploy it
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Brooklyn888/BYPASS-BOT-V2)
+
+- Don't forget to enable worker in resources tab
+- done
+
---
+++ main.py
@@ -0,0 +1,109 @@
+import telegram.ext
+import os
+
+with open('token.txt', 'r') as f:
+ TOKEN = f.read()
+
+def start(update, context):
+    update.message.reply_text("Hello Bro.I am Bypass Bot.Made with Love By Bruce :@bruce_3_33")
+    
+def help(update, context):
+   update.message.reply_text("""
+   the following commands are available
+/start -> Welcome Message
+
+/help -> all helpful command list
+
+/adf -> bypass adf.ly link
+
+/droplink -> bypass droplink.co link
+
+/gp -> bypass gplink stinky url
+
+/lv -> linkvertise link bypass
+
+/sd -> AppDrive or DriveApp links (login required wont work for now)
+
+/ex -> use this command for these services
+linkvertise.com
+exe.io/exey.io
+sub2unlock.net/sub2unlock.com
+rekonise.com
+letsboost.net
+ph.apps2app.com
+mboost.me
+shortconnect.com
+sub4unlock.com
+ytsubme.com
+bit.ly
+social-unlock.com
+boost.ink
+goo.gl
+shrto.ml
+t.co
+tinyurl.com
+
+usage - commands{} link{https://...} (example -->/adf https://adf.ly/xyz)
+   """)
+
+def adf(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python bypas.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"Link Bypassed")
+        update.message.reply_text(f"{zkm}")
+def ex(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python ex.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def gp(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python gp.py')
+        update.message.reply_text(f"Link Bypassed")
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def droplink(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python droplink.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def sd(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python sd.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+def lv(update, context):
+        zipk = context.args[0]
+        open('1.txt','w').write(zipk)
+        update.message.reply_text(f"Bypassing The Link")
+        os.system('python ex.py')
+        zkm = open('2.txt', 'r').read()
+        update.message.reply_text(f"{zkm}")
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+disp.add_handler(telegram.ext.CommandHandler("help", help))
+disp.add_handler(telegram.ext.CommandHandler("adf", adf))
+disp.add_handler(telegram.ext.CommandHandler("droplink", droplink))
+disp.add_handler(telegram.ext.CommandHandler("sd", sd))
+disp.add_handler(telegram.ext.CommandHandler("lv", lv))
+disp.add_handler(telegram.ext.CommandHandler("gp", gp))
+disp.add_handler(telegram.ext.CommandHandler("ex", ex))
+updater.start_polling()
+updater.idle()
